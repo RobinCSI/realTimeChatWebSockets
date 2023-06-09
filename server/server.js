233b35fd0c96349bbe7aeb48665b69205ce88ef1
RobinCSI <126656636+RobinCSI@users.noreply.github.com>
2023-06-10 00:10:28 +0530

    Update server.js
    Changed origin to https://real-time-chat-web-sockets-f.onrender.com/

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 // instantiating Server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://real-time-chat-web-sockets-f.onrender.com/",
     methods: ["GET", "POST"],
   },
 });
